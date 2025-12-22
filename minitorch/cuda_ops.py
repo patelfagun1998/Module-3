7cf43b0de292b1bc38ccec11a31a7162c03eb730
patelfagun1998 <patelfagun98@gmail.com>
2025-12-22 00:05:03 -0500

    cuda map

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -173,8 +173,15 @@ def tensor_map(
         out_index = cuda.local.array(MAX_DIMS, numba.int32)
         in_index = cuda.local.array(MAX_DIMS, numba.int32)
         i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
-        # TODO: Implement for Task 3.3.
-        raise NotImplementedError("Need to implement for Task 3.3")
+
+        if i < out_size:
+            to_index(i, out_shape, out_index) 
+            broadcast_index(out_index, out_shape, in_shape, in_index) 
+            out_pos = index_to_position(out_index, out_strides)
+            in_pos = index_to_position(in_index, in_strides)
+
+            out[out_pos] = fn(in_storage[in_pos])
+
 
     return cuda.jit()(_map)  # type: ignore
 
