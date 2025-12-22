010f03f39b28c3df229f5dc896e4d1d5900e26c9
patelfagun1998 <patelfagun98@gmail.com>
2025-12-22 01:23:38 -0500

    fix cuda bug

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -350,6 +350,7 @@ def tensor_reduce(
         while stride > 0:
             if pos < stride and pos+stride < reduce_size:
                 cache[pos] = fn(cache[pos], cache[pos+stride])
+            stride //= 2
             cuda.syncthreads()
         
         if pos == 0:
